0a68f7a0fbb5f3a273f0f041173e51c1f50e6a23
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-27 12:39:01 -0300

    Implementing the DAO for our CRUD implementation.

---
+++ app/src/main/java/com/example/mysubscribers/data/db/dao/SubscriberDAO.kt
@@ -0,0 +1,35 @@
+package com.example.mysubscribers.data.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mysubscribers.data.db.entity.SubscriberEntity
+
+//passo 2 do room
+@Dao
+interface SubscriberDAO {
+//implementação do CRUD
+
+
+    @Insert
+    suspend fun insert(subscriber: SubscriberEntity): Long
+
+    @Update
+    suspend fun update(subscriber: SubscriberEntity)
+
+    @Query("DELETE FROM  subscriber WHERE id = :id")
+    suspend fun delete(id: Long)
+
+    @Query("DELETE  FROM subscriber")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber")
+    suspend fun getAll(): List<SubscriberEntity>
+
+
+
+
+
+}
